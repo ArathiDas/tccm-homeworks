5d900294059865172c533864aa8d018948cb5750
ArathiDas <arathidas123321@gmail.com>
2024-12-20 21:07:45 +0100

    Update mp2_energy.c - one electron integral
    Reading one electron integral and printing the input values

--- project1/mp2_energy.c
+++ project1/mp2_energy.c
@@ -71,4 +71,39 @@ int main()
 
 }
 
+//--------------------------------------------------------------------------------//
+//                          READING ONE-ELECTRON INTEGRALS                        //
+//--------------------------------------------------------------------------------//
+
+        // Allocate memory for the data array
+        double* data = (double*)malloc(mo_num * mo_num * sizeof(double));
+        if (data == NULL)
+        {
+                printf("Memory allocation failed!\n");
+                return -1;
+        }
+
+        rc = trexio_read_mo_1e_int_core_hamiltonian(file, data);
+        if (rc != TREXIO_SUCCESS)
+        {
+                printf("TREXIO Error reading Number of up spin electrons:\n%s\n",trexio_string_of_error(rc));
+                exit(1);
+        }
+//--------------------------------------------------------------------------------//
+//                              PRINTING THE INPUT VALUE                          //
+//--------------------------------------------------------------------------------//
+
+
+    printf("Nuclear Repulsion Energy: %.6f Hartree\n", energy);
+    printf("Number of up spin electrons: %d \n", n_up);
+    printf("Number of molecular orbitals: %d \n", mo_num);
+
+    for(int i=0; i<mo_num; i++)
+    {
+            for(int j=0; j<mo_num; j++)
+            {
+                    printf("The one electron integral [%d][%d]=%f \n ",i,j,data[i*mo_num + j]);
+            }
+    }
+
 
