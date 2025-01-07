5ba2fe0e0b5b6ae98b2098de6a0bdcb41a088d82
Alen Binu Abraham <alenbinuabraham10@gmail.com>
2025-01-07 19:18:01 +0100

    Updated utils.h

--- project3/src/utils.h
+++ project3/src/utils.h
@@ -31,5 +31,8 @@ double Total_energy( double V, double T);
 
 // Function compute to the acceleration of the atoms
 void compute_acc(size_t Natoms, double** coord, double* mass, double** distance, double** acceleration, double sigma, double epsilon);
+
+// The verlet algorithm 
+void verlet_update(size_t Natoms, double dt, double** coord, double** velocity, double** acceleration, double** distance, double* mass, double sigma, double epsilon)
 #endif
 
